844336990771188d1094d57c8d1b28de5e9e1538
Komissarov Gleb <gl.k@aol.com>
2022-04-17 02:08:12 +0300

    Added MemorySegment dao implementation w/o concurrency

---
+++ src/main/java/com/lutalic/dao/PeekIterator.java
@@ -0,0 +1,42 @@
+package com.lutalic.dao;
+
+import jdk.incubator.foreign.MemorySegment;
+import com.lutalic.BaseEntry;
+
+import java.util.Iterator;
+
+public class PeekIterator implements Iterator<BaseEntry<MemorySegment>> {
+
+    private final long index;
+
+    private BaseEntry<MemorySegment> current;
+    private final Iterator<BaseEntry<MemorySegment>> iterator;
+
+    public PeekIterator(Iterator<BaseEntry<MemorySegment>> iterator, long index) {
+        this.index = index;
+        this.iterator = iterator;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return current != null || iterator.hasNext();
+    }
+
+    @Override
+    public BaseEntry<MemorySegment> next() {
+        BaseEntry<MemorySegment> next = peek();
+        current = null;
+        return next;
+    }
+
+    public BaseEntry<MemorySegment> peek() {
+        if (current == null) {
+            current = iterator.next();
+        }
+        return current;
+    }
+
+    public long getIndex() {
+        return index;
+    }
+}
